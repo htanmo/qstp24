e66cee021375e182cf256d87e190a6b4478f8d79
htanmo <145841395+htanmo@users.noreply.github.com>
2024-07-28 15:03:11 +0530

    week2: move_turtlebot node done

--- src/week2/week2/move_turtlebot.py
+++ src/week2/week2/move_turtlebot.py
@@ -6,7 +6,7 @@ class  MoveTurtleBot(Node):
     def __init__(self):
         super().__init__('move_turtlebot')
         self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
-        self.timer_ = self.create_timer(0.1, timer_callback)
+        self.timer_ = self.create_timer(0.1, self.timer_callback)
         self.vel_msg = Twist()
         self.vel_msg.linear.x = 0.5
         self.get_logger().info("Move TurtleBot Publisher started.")
