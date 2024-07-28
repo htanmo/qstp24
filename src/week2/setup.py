1be1762522920a48c669c3b780860e768f7b2df5
htanmo <145841395+htanmo@users.noreply.github.com>
2024-07-28 14:38:03 +0530

    created: move_turtlebot/ node

--- src/week2/setup.py
+++ src/week2/setup.py
@@ -1,25 +1,25 @@
 from setuptools import find_packages, setup
 
-package_name = 'week2'
+package_name = "week2"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='htanmo',
-    maintainer_email='145841395+htanmo@users.noreply.github.com',
-    description='TODO: Package description',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="htanmo",
+    maintainer_email="145841395+htanmo@users.noreply.github.com",
+    description="TODO: Package description",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "move_turtlebot = week2.move_turtlebot:main",
         ],
     },
 )
---
+++ src/week2/week2/move_turtlebot.py
@@ -0,0 +1,26 @@
+import rclpy
+import rclpy.node import Node
+from geometry_msgs.msg import Twist 
+
+class  MoveTurtleBot(Node):
+    def __init__(self):
+        super().__init__('move_turtlebot')
+        self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.timer_ = self.create_timer(0.1, timer_callback)
+        self.vel_msg = Twist()
+        self.vel_msg.linear.x = 0.5
+        self.get_logger().info("Move TurtleBot Publisher started.")
+    
+    def timer_callback(self):
+        self.publisher_.publish(self.vel_msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    move_turtlebot = MoveTurtleBot()
+    rclpy.spin(move_turtlebot)
+    move_turtlebot.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
